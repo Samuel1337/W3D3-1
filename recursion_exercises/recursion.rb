f74535ded33d4df1aa16ce2be588801971670a9f
Mini-26 <app@gmail.com>
2022-03-09 13:02:44 -0800

    w3d3 first commit

---
+++ recursion_exercises/recursion.rb
@@ -0,0 +1,126 @@
+=begin
+def iter_range(start, en)
+    array = []
+    start.step(en) { |num| array << num }
+    array
+end
+
+def rec_range(start, en)
+    return [] if en < start
+    array = [start]
+    array += rec_range(start+1, en)
+    return array
+end
+
+p iter_range(0, 10)
+p rec_range(0, 10)
+
+
+def exp1(b, n)
+    return 1 if n == 0
+    b * exp1(b, n-1)
+end
+
+p exp1(2, 5)
+
+def exp2(b, n)
+    return 1 if n == 0
+    if n.even?
+        half = exp2(b, n/2)
+        return half * half
+    else
+        return b * exp2(b, n-1)
+    end
+end 
+
+p exp2(2, 5)
+p exp2(2, 6)
+
+
+class Array
+    
+    def deep_dup
+        self.map do |el|  
+            if el.is_a?(Array)
+                el.deep_dup
+            else
+                el
+            end
+        end
+    end
+    
+end
+
+def iter_fibs(n)
+    arr = [0, 1]
+    while arr.length < n 
+        arr << arr[-1] + arr[-2]
+    end
+    arr[0..n-1]
+end
+
+def rec_fibs(n)
+    return [] if n == 0
+    return [0] if n == 1
+    return [0,1] if n == 2
+    prev = rec_fibs(n-1)
+    prev << prev[-1] + prev[-2]
+end
+
+p iter_fibs(5)
+p rec_fibs(5)
+
+
+
+def bsearch(arr, target)
+    return nil if arr.length == 0
+    mid = arr.length / 2
+    return mid if arr[mid] == target
+    if target > arr[mid]
+        right = bsearch(arr[mid + 1..-1], target)
+        return right != nil ? mid + 1 + right : nil
+    else
+        return bsearch(arr[0...mid], target)
+    end
+end
+
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+
+
+def merge(arr1, arr2)
+    new_arr = []
+    while arr1.length > 0 || arr2.length > 0
+        if arr1.length == 0
+            new_arr << arr2.shift
+        elsif arr2.length == 0
+            new_arr << arr1.shift
+        elsif arr1[0] < arr2[0]
+            new_arr << arr1.shift
+        else
+            new_arr << arr2.shift
+        end
+    end
+    new_arr
+end
+
+def merge_sort(arr)
+    return arr if arr.length <= 1
+    mid = arr.length / 2
+    left = merge_sort(arr[0...mid])
+    right = merge_sort(arr[mid..-1])
+    merge(left, right)
+end
+
+p merge_sort([1, 3, 2, 9, 10, 1, 2, 3, 5, 7])
+
+=end
+
+def subsets(array)
+    
+end
